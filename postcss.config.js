e50f2c50f4b077f3053a6f665d2699c7c02dc9c0
勿把十年读成伤 <1040418011@qq.com>
2019-06-26 00:14:22 +0800

    add sourcemap

---
+++ postcss.config.js
@@ -0,0 +1,5 @@
+module.exports = {       //对厂商前缀 插件的配置，不提倡这种繁琐的写法。直接在options中配置即可
+    plugins:[
+        require('autoprefixer')
+    ]
+  }
---
+++ src/js/index.js
@@ -0,0 +1,68 @@
+require('../css/index.css');        //两种模块化规范都可以
+import '../css/myStylus.styl';      //两种模块化规范都可以
+const $ = require('jquery');
+
+$('.tab-item').on('click', function (e) {
+
+    $('.tab-item').removeClass('tab-item-actve')
+    $(e.currentTarget).addClass('tab-item-actve')
+
+    var type = e.target.getAttribute('type')
+    myScrollTo(type, 500)
+})
+function myScrollTo(type) {
+    var myIndex;
+    var heights = [];
+    [].slice.call($('.card-aline')).forEach(function(ele, index){
+        if (ele.id == type) {
+            myIndex = index;
+        }
+        heights.push($(ele).height())
+    })
+    window.allHeight = heights.reduce(function(a,b){return a+b})    //滚动内容总高，用于计算可滚动高度
+    var arr = heights.slice(0, myIndex)
+    var y;
+    y = arr.length != 0 ? arr.reduce(function (a, b)  {return a + b}) : 0
+    scrollStart(y)
+}
+
+window.timer = '';
+
+function scrollStart(y) {
+    clearInterval(timer);
+    var isHeight = $('.scroll-wrap').height()
+    var k = allHeight - isHeight     //最高滚动距离，总高度减去盒子高度
+
+    // var child = [].slice.call($('.card-aline'))
+    // var lastChild = child[child.length -1]
+    // var lastChildheight = $(lastChild).height()
+    // var k = isHeight - lastChildheight
+
+    var el = document.getElementsByClassName('scroll-wrap')[0]
+    var currentPosition = el.scrollTop ;
+
+    timer = setInterval(function() {
+        var el = document.getElementsByClassName('scroll-wrap')[0]
+        var currentPosition = el.scrollTop ;
+        var curPos;
+        if(Math.abs(currentPosition - y) < 7){
+            clearInterval(timer);
+            $('.scroll-wrap').scrollTop(y);
+            console.log('滚完了')
+        }else{
+            curPos = currentPosition > y ? currentPosition - 7 : currentPosition + 7;
+            if(curPos > k){    //如果大于最大可滚动高度
+                clearInterval(timer);
+                console.log('超过滚动高度，老子不滚啦！')
+                $('.scroll-wrap').scrollTop(k);
+            }else{
+                $('.scroll-wrap').scrollTop(curPos);
+            }
+        }
+    },1);
+}
+
+
+
+
+consols.log(1)
