32d2f1e639ebe715796053763a5bc696c25d27b0
TianXiaoShang <1040418011@qq.com>
2019-06-28 09:09:46 +0800

    0

--- webpack.config.js
+++ webpack.config.js
@@ -17,20 +17,20 @@ module.exports = {
     output: {
         // publicPath:'http://cdn.com.cn',                   //当js在另一个cdn地址上，这里将作为html引用js地址中拼接成最终的完整访问地址；如：<script src="http://cdn.com.cn/js/index.boundle.js"></script>;
         path: path.resolve(__dirname, 'dist'),              //dirname代表当前配置文件所在的目录，也就是根目录，在此根目录下创建子文件夹dist为打包后的文件路径；
-        filename: "js/[name]_[hash].boundle.js"           //除了命名，可以在前面加路径名，创建文件夹，增加相对打包出来的dist的路径；
+        filename: "js/[name]_[hash].boundle.js"             //除了命名，可以在前面加路径名，创建文件夹，增加相对打包出来的dist的路径；
     },
     // mode:"production",       //生产版本，代码将被压缩(默认为production)
     mode: "development",         //开发版本，代码不被压缩；
     devtool: 'cheap-module-eval-source-map',         //源映射;在打包后的代码中对源代码进行映射便于报错时候的行数快速定位
     // devtool: 'cheap-module-source-map',           //生产环境推荐用这种
-    //总结来讲，development推荐cheap-module-eval-source-map，production推荐cheap-module-source-map（线上环境有正确的报错也便于调试，当然如果你完全不需要在线上定位报错则用none或者直接注释掉即可）
-    //值得注意的是，不管使用哪种模式，当有产生.map文件时，.map文件只会在F12开启时进行下载（sourceMap主要服务于调试），所以其实更推荐使用单独生成.map文件的形式。否则放到js中会增加代码体积，也就是说避免使用inline
-    //none，不触发sourceMap（拓展：https://www.cnblogs.com/chris-oil/p/8856020.html）
-    //source-map进行源代码映射可以在报错中正确显示其在源代码中的行数，但会导致打包速度变慢，因为需要生成映射map文件
-    //inline-source-map,跟source-map唯一不同的是不会单独生成map文件，会以base64格式放在打包后的js中；
-    //cheap-inline-source-map,加上cheap则只会告诉你第几行，不会告诉你第几列（或者说第几个字符）出错，省性能；
-    //cheap-module-inline-source-map,加上module代表不止管业务代码，依赖的包中的报错也能帮你定位；
-    //eval，不生成map文件，以eval的方式生成对应关系，性能最佳，但是提示信息不够全；
+        //总结来讲，development推荐cheap-module-eval-source-map，production推荐cheap-module-source-map（线上环境有正确的报错也便于调试，当然如果你完全不需要在线上定位报错则用none或者直接注释掉即可）
+        //值得注意的是，不管使用哪种模式，当有产生.map文件时，.map文件只会在F12开启时进行下载（sourceMap主要服务于调试），所以其实更推荐使用单独生成.map文件的形式。否则放到js中会增加代码体积，也就是说避免使用inline
+        //none，不触发sourceMap（拓展：https://www.cnblogs.com/chris-oil/p/8856020.html）
+        //source-map进行源代码映射可以在报错中正确显示其在源代码中的行数，但会导致打包速度变慢，因为需要生成映射map文件
+        //inline-source-map,跟source-map唯一不同的是不会单独生成map文件，会以base64格式放在打包后的js中；
+        //cheap-inline-source-map,加上cheap则只会告诉你第几行，不会告诉你第几列（或者说第几个字符）出错，省性能；
+        //cheap-module-inline-source-map,加上module代表不止管业务代码，依赖的包中的报错也能帮你定位；
+        //eval，不生成map文件，以eval的方式生成对应关系，性能最佳，但是提示信息不够全；
 
     module: {
         rules: [                                     //众所周知use中的loader是倒着用的，所以顺序一定要注意；
@@ -51,13 +51,18 @@ module.exports = {
             {   //file-loader
                 test: /\.(eot|ttf|svg|woff|woff2)$/, //字体文件用file-loader
                 use: {
-                    loader:'file-loader'             //只需要使用file转移到输出目录即可
+                    loader:'file-loader',            //只需要使用file转移到输出目录即可
+                    options:{
+                        // publicPath:'',
+                        outputPath:'font/'           //打包后创建并放入一个font的文件夹中便于管理
+                    }
                 }
             },
             {   //stylus-loader
                 test: /\.styl$/,                     //stylus文件编译成普通css文件(npm i stylus stylus-loader --save)
                 use: [
-                    'style-loader',
+                    // 'style-loader',
+                    MiniCssExtractPlugin.loader,     //一般我们都单独抽离css文件
                     {
                         loader: 'css-loader',
                         options: {
@@ -65,22 +70,23 @@ module.exports = {
                             // modules: true        //使用模块化css，使用之后需要通过import style from 'xxx';img.calssList.add(style.avatar)这样的方式针对局部使用该样式文件；目的是不影响全局样式，鸡肋！
                         }
                     },
-                    'stylus-loader',                //npm i stylus stylus-loader --save
-                    'postcss-loader'                //这种写法需要另建postcss.config.js文件来配置，不如上方直接在options中配置
+                    'postcss-loader',                //这种写法需要另建postcss.config.js文件来配置，不如上方直接在options中配置
+                    'stylus-loader'                  //注意顺序，postcss-loader在前，先要解析stylus，再加厂商前缀    npm i stylus stylus-loader --save   
                 ]
             },
             {   //sass-loader
                 test: /\.scss$/,                    //scss编译，同理stylus！
                 use: [
-                    'style-loader',
+                    // 'style-loader',
+                    MiniCssExtractPlugin.loader,     
                     {
                         loader: 'css-loader',
                         options: {
                             importLoaders: 2,       
                         }
                     },
-                    'sass-loader',                  
-                    'postcss-loader'              
+                    'postcss-loader',             
+                    'sass-loader'                 
                 ]
             },
             {   //url-loader   内置了file-loader，当超过limit规定的大小，则使用file-loader，会将匹配的文件复制到规定的imgs目录
@@ -89,8 +95,8 @@ module.exports = {
                     loader: 'url-loader',
                     options: {
                         outputPath: 'imgs/',                //图片的输出地址，相对于output.path的地址（也就是输出文件夹下创建一个imgs文件夹存放图片）
-                        publicPath: '../imgs/',             //引用该文件的路径。
-                        limit: 8 * 1024,                    //限制文件的大小 8k
+                        publicPath: '../imgs/',             //引用该文件的路径。（打包后去哪找图片！）
+                        limit: 2 * 1024,                    //限制文件的大小 8k
                         name: '[name]_[hash].[ext]'         //打包后的命名规则
                     }
                 }
@@ -155,7 +161,7 @@ module.exports = {
         }),
         new WebpackDeepScopeAnalysisPlugin(),
         new CleanWebpackPlugin(),                       //用于在重新打包时删除原有代码（主要解决带hash文件没法删除的问题），同时在运行dev服务时也会删除输出目录代码，此时运行的代码在内存中也看不到；
-        new Webpack.HotModuleReplacementPlugin(),       //开启热更新 
+        new Webpack.HotModuleReplacementPlugin(),       //开启热更新，也就是传说中的HMR
         // -->对于css，样式改变不需要刷新页面，自动替换；
         // -->对于js，可以在入口文件中对不同模块进行监听处理；如下代码（index.js）：
         // 总结来讲对css是神器，对js略鸡肋，但事实上css只是css-loader帮我们把下面这种繁琐的事情做了，而js只能手写。而在vue中vue-loader也帮我们做了这种事。不需要手动写callback；
